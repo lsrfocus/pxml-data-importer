236ea0cef3303e943a6ab391dee68a22de3cc722
Kevin Cooper <cooperka92@gmail.com>
2019-03-05 17:10:19 -0500

    Add import/export instructions to readme

--- .neo4j/data/setup-neo4j.sh
+++ .neo4j/data/setup-neo4j.sh
@@ -5,8 +5,12 @@ set -exo pipefail
 #
 # For reference, see https://neo4j.com/docs/operations-manual/current/tools/dump-load/
 # and https://stackoverflow.com/questions/45166636/create-neo4j-databse-from-backup-inside-neo4j-docker
-#
+
 # Example (uncomment to export your local database):
 #
 #DATE_FORMAT=$(date +%Y-%m-%d-%H-%M-%S)
 #bin/neo4j-admin dump --database=graph.db --to=data/db-${DATE_FORMAT}.dump
+
+# Example (uncomment to import):
+#
+#bin/neo4j-admin load --from=my-db.dump --database=graph.db
--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Setup:
 - `(cd .neo4j/plugins/ && curl -LO https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.5.0.2/apoc-3.5.0.2-all.jar)`
 - `docker-compose -f db.yml up -d palantir-import`
 - Open <http://localhost:7474/browser/> to set your password (default is `neo4j`, python script expects `pwd`)
+
+Import/export data:
+- Uncomment the appropriate lines in `.neo4j/data/setup-neo4j.sh`
+- `docker-compose -f db.yml restart palantir-import`
